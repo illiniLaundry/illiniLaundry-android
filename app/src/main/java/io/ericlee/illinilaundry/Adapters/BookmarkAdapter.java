48e988d89ff681555ff6303721dd6cf23752d25b
dl-eric <orangepamelo@gmail.com>
2016-06-30 04:25:35 -0400

    Refactor code to access contexts better

--- app/src/main/java/io/ericlee/illinilaundry/Adapters/BookmarkAdapter.java
+++ app/src/main/java/io/ericlee/illinilaundry/Adapters/BookmarkAdapter.java
@@ -63,9 +63,9 @@ public class BookmarkAdapter extends RecyclerView.Adapter<BookmarkAdapter.ViewHo
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.getContext(), DormActivity.class);
+                Intent intent = new Intent(v.getContext(), DormActivity.class);
                 intent.putExtra("Dorm", dorm);
-                MainActivity.getContext().startActivity(intent);
+                v.getContext().startActivity(intent);
             }
         });
 
